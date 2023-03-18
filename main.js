ff145190d08fa38b8657308b71ef3a0115258fc0
M4x <maxmurray1999@gmail.com>
2023-03-18 12:50:34 +0000

    Added random Location and Married Status

--- main.js
+++ main.js
@@ -6,13 +6,22 @@
 
 // Call that function 3 times with 3 different values for the arguments.
 
+function tellFortune(firstName, age, married) {
 
-function tellFortune(firstName, age,) {
+    if(married === true) {
+        relationshipStatus = ["You will be with your current partner", "You Won't be with your current partner"]
+    } else relationshipStatus = ["You will have found love", "You won't have found love"]
+
+    let gotWifey = relationshipStatus[Math.floor(Math.random() * relationshipStatus.length)]
 
     let newAge = age + Math.floor(Math.random() * (5,40));
 
-    let nameList = ["Barry", "Tony", "ShaNayNay", "Trevington"]
+    let nameList = ["Tootlepip", firstName+"ington", "ShaNayNay", "Mc"+firstName+"Face"]
     let newName = nameList[Math.floor(Math.random() * nameList.length)];
-    return `${firstName} is your current name you are currently ${age} years old. However, by the time you are ${newAge}, unfortunately - you will have changed your name to ${newName} and you will have become a Chicken Nugget`
+
+    let locationList = ["Bali", "London" , "Australia", "Texas", "Bourton-On-The-Water"]
+    let newLocation = locationList[Math.floor(Math.random() * locationList.length)];
+
+    return `${firstName} - by the time you are ${newAge}, unfortunately - you will have changed your name to ${newName}. However, you will live in ${newLocation}. Lastly, ${gotWifey}`
 
 }
